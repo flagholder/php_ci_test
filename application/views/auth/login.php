9167bc732d53b0b9fb4e82e0b126e0f47611f88a
flagholder <shenborui@hotmail.com>
2017-03-06 00:06:38 -0800

    modify login view

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -15,20 +15,20 @@
                 <div class="col-sm-9">
                     <div class="input-group">
                         <span class="input-group-addon"><i class="fa fa-user fa-fw" aria-hidden="true"></i></span>
-                        <input type="text" class="form-control" id="yourName" placeholder="Email or username">
+                        <input type="text" class="form-control" id="login" placeholder="Your register email">
                     </div>
                 </div>
             </div>
 
-            <div class="form-group">
-                <label for="yourEmail" class="control-label col-sm-3">Email address</label>
-                <div class="col-sm-9">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></span>
-                        <input type="email" class="form-control" id="yourEmail" placeholder="Your Email Id">
-                    </div>
-                </div>
-            </div>
+<!--            <div class="form-group">-->
+<!--                <label for="yourEmail" class="control-label col-sm-3">Email address</label>-->
+<!--                <div class="col-sm-9">-->
+<!--                    <div class="input-group">-->
+<!--                        <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></span>-->
+<!--                        <input type="email" class="form-control" id="yourEmail" placeholder="Your Email Id">-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
 
             <div class="form-group">
                 <label for="password" class="control-label col-sm-3">Password</label>
@@ -40,30 +40,12 @@
                 </div>
             </div>
 
-            <div class="form-group">
-                <label for="yourComments" class="control-label col-sm-3">Tell us</label>
-                <div class="col-sm-9">
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="fa fa-tags fa-fw" aria-hidden="true"></i></span>
-                        <textarea class="form-control" id="yourComments" placeholder="Your commments"
-                                  rows="3"></textarea>
-                    </div>
-                </div>
-            </div>
-
-            <!-- File Button -->
-            <div class="form-group">
-                <label class="control-label col-sm-3" for="Icon">Icon</label>
-                <div class="col-sm-9">
-                    <input id="Icon" name="Icon" class="input-file" type="file">
-                </div>
-            </div>
 
             <div class="clearfix form-group">&nbsp;</div>
 
             <div class="text-center col-sm-12 form-group">
                 <button type="submit" class="btn btn-info">
-                    <i class="fa fa-envelope fa-fw"></i>&nbsp;Submit
+                    <i class="fa fa-envelope fa-fw"></i>&nbsp;SIGN IN
                 </button>
             </div>
         </form>
